6a931bfeb6c9b2d1fde9c2d575c9be24f200dfb5
Igor <igor.ogerchuk@gmail.com>
2020-01-05 02:07:17 +0200

    deployed with netlify

---
+++ build/precache-manifest.9337edce9745fa1f80ec0c4f4f80a2e5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc1cafa22de679f26cef12664b17cba6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c26b76c40a379dd52a0",
+    "url": "/static/js/2.b12f4e17.chunk.js"
+  },
+  {
+    "revision": "0a858fcfa7d027be33cc",
+    "url": "/static/js/main.ef4d48aa.chunk.js"
+  },
+  {
+    "revision": "44ac136ad102d14130b4",
+    "url": "/static/js/runtime-main.7008c063.js"
+  }
+]);
